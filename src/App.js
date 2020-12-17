566710c8be2d762ddb20cc2f2f999e9152101eb9
CAP <cap@cap.com>
2020-12-16 23:19:45 -0300

    Se se agregan funciones y se modifica html y CSS

--- src/App.js
+++ src/App.js
@@ -1,29 +1,83 @@
-import React, { useState } from "react";
+import React from "react";
+import {useState} from "react";
+
+// Import css bootstrap and fontawesome
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+// Import JS bootstrap jquery and popper.js
+import "bootstrap";
 import "./styles/App.css";
 
-const Lista = (props) => {
-  const [state, setState] = useState({
-    label: ["Tarea 1","Tarea 2"],
-  });
-
+function DespliegeTarea({ todo, index, deleteTarea }) {
   return (
-    <div>
-      <input
-        onKeyUp={(e) =>
-          e.key === "Enter" && setState({ ...state, label: [...state.label, e.target.value] })
-        }
-      />
-      <ul>
-        {state.label.length > 0 ? (
-          state.label.map((element, index) => {
-            return <li key={index}>{element}</li>;
-          })
-        ) : (
-          "Loading...."
-        )}
-      </ul>
+    <div className="todo">
+      {todo.text}
+      <div>
+        <button
+          type="button"
+          className="btn-close btn-close-black"
+          aria-label="Close"
+          onClick={() => deleteTarea(index)}
+        >
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
     </div>
   );
-};
+}
 
-export default Lista;
+function Formulario({ agregarTarea }) {
+  const [value, setState] = useState("");
+
+  const Submit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    agregarTarea(value);
+    setState("");
+  };
+
+  return (
+    <form onSubmit={Submit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={(e) => setState(e.target.value)}
+      />
+    </form>
+  );
+}
+
+function App2() {
+  const [todos, setState] = useState([
+    {
+      text: "Tarea 1",
+    },
+  ]);
+
+  const agregarTarea = (text) => {
+    const newTarea = [...todos, { text }];
+    setState(newTarea);
+  };
+
+  const deleteTarea = (index) => {
+    const newTarea = [...todos];
+    newTarea.splice(index, 1);
+    setState(newTarea);
+  };
+
+  return (
+    <div className="app">
+      <div className="container listaCompleta">
+        <Formulario agregarTarea={agregarTarea} />
+        <br></br>
+        {todos.map((todo, index) => (
+          <DespliegeTarea key={index} index={index} todo={todo} deleteTarea={deleteTarea} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App2;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Lista from './App';
+import App from './App';
 
-ReactDOM.render(<Lista />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root')); 
